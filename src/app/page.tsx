6ceb6cebe5276e29599fca6d55d7a489f6717fe0
Derick Moncado <derickmoncado@gmail.com>
2026-01-08 20:14:33 -0800

    Messin with carousel and getting it set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import useEmblaCarousel from "embla-carousel-react";
+
 export default function Home() {
+	const [emblaRef] = useEmblaCarousel({ loop: true });
+
 	return (
 		<div className="container">
 			<h1>Home</h1>
 			<p>some placeholder text</p>
+			==========================================================
+			<div className="embla" ref={emblaRef}>
+				<div className="embla__container">
+					<div className="embla__slide">Slide 1</div>
+					<div className="embla__slide">Slide 2</div>
+					<div className="embla__slide">Slide 3</div>
+				</div>
+			</div>
 		</div>
 	);
 }

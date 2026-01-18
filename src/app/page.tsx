d502027d952392859e71813aaa011c6cfe2911b4
Derick Moncado <derickmoncado@gmail.com>
2026-01-17 16:26:04 -0800

    styling and placeholder content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 
 import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
-import { ArrowBigDown } from "lucide-react";
+import { ArrowBigDown, Smile, Package, Inbox, Menu, CircleCheck, Send } from "lucide-react";
+import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
@@ -11,10 +12,7 @@ export default function Home() {
 	const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
 	const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
 
-	const [emblaRef, emblaApi] = useEmblaCarousel(
-		{ axis: "x", direction: "ltr", loop: true, align: "center" },
-		[Autoplay({ delay: 4000, stopOnInteraction: false })]
-	);
+	const [emblaRef, emblaApi] = useEmblaCarousel({ axis: "x", direction: "ltr", loop: true, align: "center" }, [Autoplay({ delay: 4000, stopOnInteraction: false })]);
 
 	const onSelect = useCallback((api: typeof emblaApi) => {
 		if (!api) return;
@@ -30,10 +28,7 @@ export default function Home() {
 
 	const scrollPrev = useCallback(() => emblaApi?.scrollPrev(), [emblaApi]);
 	const scrollNext = useCallback(() => emblaApi?.scrollNext(), [emblaApi]);
-	const scrollTo = useCallback(
-		(index: number) => emblaApi?.scrollTo(index),
-		[emblaApi]
-	);
+	const scrollTo = useCallback((index: number) => emblaApi?.scrollTo(index), [emblaApi]);
 
 	useEffect(() => {
 		if (!emblaApi) return;
@@ -54,7 +49,7 @@ export default function Home() {
 			<br />
 			<p>
 				<ArrowBigDown />
-				&nbsp; here is a carousel
+				&nbsp; here is a carousel from <Link href="https://www.embla-carousel.com/">Embla Carousel</Link>
 			</p>
 			<br />
 			<section className="embla" ref={emblaRef}>
@@ -66,37 +61,34 @@ export default function Home() {
 			</section>
 			<div className="embla__controls">
 				<div className="embla__buttons">
-					<button
-						className="embla__button embla__button--prev"
-						type="button"
-						onClick={scrollPrev}
-						disabled={!prevBtnEnabled}
-					>
+					<button className="embla__button embla__button--prev" type="button" onClick={scrollPrev} disabled={!prevBtnEnabled}>
 						Prev
 					</button>
-					<button
-						className="embla__button embla__button--next"
-						type="button"
-						onClick={scrollNext}
-						disabled={!nextBtnEnabled}
-					>
+					<button className="embla__button embla__button--next" type="button" onClick={scrollNext} disabled={!nextBtnEnabled}>
 						Next
 					</button>
 				</div>
 				<div className="embla__dots">
 					{scrollSnaps.map((_, index) => (
-						<button
-							key={index}
-							className={`embla__dot${
-								index === selectedIndex ? " embla__dot--selected" : ""
-							}`}
-							type="button"
-							onClick={() => scrollTo(index)}
-							aria-label={`Go to slide ${index + 1}`}
-						/>
+						<button key={index} className={`embla__dot${index === selectedIndex ? " embla__dot--selected" : ""}`} type="button" onClick={() => scrollTo(index)} aria-label={`Go to slide ${index + 1}`} />
 					))}
 				</div>
 			</div>
+			<br />
+			==========================================================
+			<br />
+			<br />
+			<p>
+				And here are some icons from <Link href="https://www.embla-carousel.com/">Lucide Icons</Link>
+			</p>
+			<p>
+				<Smile /> <Package /> <Inbox /> <Menu /> <CircleCheck /> <Send />
+			</p>
+			<br />
+			==========================================================
+			<br />
+			<br />
+			<p>This is your boilerplate now, go build something cool.</p>
 		</div>
 	);
 }
